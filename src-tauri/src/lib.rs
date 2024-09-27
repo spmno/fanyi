8d9c746e9967311dd88fd81978917824d17bd546
spmno <sunqingpeng@hotmail.com>
2024-09-27 11:34:11 +0800

    complete logic part.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,8 +2,12 @@
 use std::thread;
 use rdev::{listen, Event, EventType, Key};
 use clippers::Clipboard;
+use serde::{Serialize, Deserialize};
 
-static mut KEY_STORE:KeyStore;
+static mut KEY_STORE:KeyStore = KeyStore{
+    current_key : Key::KeyA,
+    privious_key : Key::KeyA
+};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -28,6 +32,74 @@ fn init_event_thread() {
     });
 }
 
+#[derive(Serialize)]
+enum TargetLang {
+    #[serde(rename(serialize = "ZH-HANS"))]
+    ZHHANS,
+    EN
+}
+
+#[derive(Serialize)]
+struct DeepLRequest {
+    text: Vec<String>,
+    target_lang: TargetLang
+}
+
+#[derive(Deserialize)]
+struct DeepLResponse {
+    translations: Vec<TranslateResult>
+}
+
+#[derive(Deserialize)]
+struct TranslateResult {
+    detected_source_language: String,
+    text: String
+}
+
+fn translate() {
+    println!("translate.");
+    let mut clipboard = Clipboard::get();
+    match clipboard.read() {
+        Some(clippers::ClipperData::Text(text)) => {
+            println!("Clipboard text: {:?}", text);
+            let client = reqwest::blocking::Client::new();
+            let url = "https://api-free.deepl.com/v2/translate";
+            let auth_key = std::env::var("DEEPL_API_KEY").unwrap();
+            let token = "DeepL-Auth-Key ".to_string() + &auth_key;
+            let req: DeepLRequest = DeepLRequest {
+                target_lang : TargetLang::ZHHANS,
+                text: vec![text.to_string()]
+            };
+            let res = client.post(url)
+                .header(reqwest::header::AUTHORIZATION, token)
+                .json(&req)
+                .send();
+            match res {
+                Ok(res) => {
+                    //println!("res: {}", res.text().unwrap());
+                    let result = res.json::<DeepLResponse>().unwrap();
+                    println!("result: {}", result.translations[0].text);
+                },
+                Err(err) => {
+                    println!("err: {}", err);
+                }
+            }
+        }
+
+        Some(clippers::ClipperData::Image(image)) => {
+            println!("Clipboard image: {}x{} RGBA", image.width(), image.height());
+        }
+
+        Some(data) => {
+            println!("Clipboard data is unknown: {data:?}");
+        }
+
+        None => {
+            println!("Clipboard is empty");
+        }
+    }
+}
+
 fn callback(event: Event) {
     match event.event_type{
         EventType::KeyPress(key) => {
@@ -35,13 +107,21 @@ fn callback(event: Event) {
                 KEY_STORE.current_key = key;
                 match key {
                     Key::KeyC => {
-
+                        if KEY_STORE.privious_key == Key::ControlLeft {
+                            translate();
+                            KEY_STORE.clear_status();
+                        }
                     },
                     Key::ControlLeft => {
-
+                        if KEY_STORE.privious_key == Key::KeyC {
+                            translate();
+                            KEY_STORE.clear_status();
+                        }
                     }
                     _ => ()
                 }
+                KEY_STORE.privious_key = KEY_STORE.current_key;
+
             }
             println!("press {:?}", key)
         },
@@ -52,4 +132,11 @@ fn callback(event: Event) {
 struct KeyStore {
     current_key: Key,
     privious_key: Key,
+}
+
+impl KeyStore {
+    fn clear_status(&mut self) {
+        self.privious_key = Key::KeyA;
+        self.current_key = Key::KeyA;
+    }
 }
